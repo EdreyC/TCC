df6f836f1ce0a32731c6c362548dc2184003a53f
Edrey Carneiro <83123090+EdreyC@users.noreply.github.com>
2022-02-22 21:42:08 -0300

    Add files via upload

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import './button.scss'
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+    
+export default function Button(props: ButtonProps){
+    return(
+        <button className="button"{...props}/>
+    )
+}
