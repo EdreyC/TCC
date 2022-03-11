3145c2af3163202e700ddedb4dc39a8fba545f7c
Gabriel B M Rocha <gb.mrochadev@gmail.com>
2022-03-11 20:05:21 -0300

    Add Services and FireBase, Start migr to bootstrap

---
+++ frontend/src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCU7O-CXv-vuzxXYBJBO4147aK5Uq-cotg",
+  authDomain: "agilizeit-ff6de.firebaseapp.com",
+  projectId: "agilizeit-ff6de",
+  storageBucket: "agilizeit-ff6de.appspot.com",
+  messagingSenderId: "1078371033969",
+  appId: "1:1078371033969:web:bbd2db16753db520c5ccd0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
