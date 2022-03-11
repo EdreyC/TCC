03425662a71686449c72f04bce2c9a9a3faff8ce
Gabriel B M Rocha <gb.mrochadev@gmail.com>
2022-03-11 19:43:31 -0300

    remove backend folder

--- backend/schema/task.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-from pydantic import BaseModel
-
-class Tarefa(BaseModel):
-    titulo: str
-    descricao: str
-    comentarios: list(str)
-    prioridade: Enum
-    status: bool
