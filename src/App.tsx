7e6638d04874bac36de8281e3789bd22c2f34f31
EdreyC <edreycarneiro.profissional@gmail.com>
2022-04-05 00:35:05 -0300

    validate signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { AuthContextProvider } from "./context/AuthContext"
 import MainRoutes from "../routes"
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { signOut } from "firebase/auth";
+import { useEffect } from "react";
 
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/navbar";
@@ -6,34 +7,40 @@ import { useAuth } from "../hooks/useAuth";
 import { auth } from "../services/firebase";
 
 export default function Home() {
+
   const navigate = useNavigate();
   const { user } = useAuth()
+
   console.log(user)
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/signin')
+    }
+
+  }, [])
+
   async function SignOut() {
 
     await signOut(auth).then(() => {
-   
+      navigate('/signin')
+
       console.log(auth.name)
 
     }).catch((error) => {
       console.log(error)
     });
-
-    navigate('/signin')
+    window.location.reload();
 
   }
 
   return (
 
     <div>
-      <Navbar img={user?.avatar}/>
 
       <button onClick={SignOut} >Logout</button>
       <h1>{user?.name}</h1>
       <img src={user?.avatar} alt="" />
-
-
-
 
     </div>
   )
