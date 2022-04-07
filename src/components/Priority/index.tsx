9f5ceabbc5f21b561957478455270610d835e2ce
EdreyC <edreycarneiro.profissional@gmail.com>
2022-04-06 21:15:42 -0300

    priority initial

---
+++ src/components/Priority/index.tsx
@@ -0,0 +1,30 @@
+const theme = {
+    colors:{
+        DoNow:"#DF2B4B",
+        High:"#FF4E6E",
+        Medium:"#FFC56D",
+        Low:"#69D26D"
+    },
+    title:{
+        DoNow:"Do Now",
+        High:"High",
+        Medium:"Medium",
+        Low:"Low"
+    }
+ }
+// type Props = {
+//     colors:{
+
+//     }
+// }
+const Priority = ()=>{
+
+        
+    return(
+        <div>
+            <h1>{}</h1>
+        </div>
+    )
+}
+export default Priority;
+
